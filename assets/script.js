08037d89d41b5e5cf419c22f283a3fff1ce1af6c
MizzZer <74468523+MizzZer@users.noreply.github.com>
2025-06-13 18:02:23 +0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,21 @@
-const faders = document.querySelectorAll('.fade-in');
 
-const appearOptions = {
-  threshold: 0.2,
-  rootMargin: '0px 0px -50px 0px'
-};
+document.addEventListener("DOMContentLoaded", () => {
+  const faders = document.querySelectorAll('.fade-in');
+  const options = {
+    threshold: 0.2,
+    rootMargin: '0px 0px -50px 0px'
+  };
 
-const appearOnScroll = new IntersectionObserver(function(
-  entries,
-  appearOnScroll
-) {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting) return;
-    entry.target.classList.add('show');
-    appearOnScroll.unobserve(entry.target);
+  const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+        appearOnScroll.unobserve(entry.target);
+      }
+    });
+  }, options);
+
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
   });
-}, appearOptions);
-
-faders.forEach(fader => {
-  appearOnScroll.observe(fader);
 });
